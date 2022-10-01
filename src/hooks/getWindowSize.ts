a47e51a83fe4c40db95fcfd4ed9ff8fb9c75b9d8
hayapo <keep.forward1001@gmail.com>
2022-10-01 18:16:27 +0900

    :art: windowサイズを取ってくるようにした

---
+++ src/hooks/getWindowSize.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+
+export const GetWindowSize = () => {
+  const [windowSizes, setWindowSize] = useState({
+    windowWidth: 0,
+    windowHeight: 0,
+  })
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleResize = () => {
+        setWindowSize({
+          windowWidth: window.innerWidth,
+          windowHeight: window.innerHeight,
+        })
+      }
+      window.addEventListener('resize', handleResize)
+      handleResize()
+      return () => window.removeEventListener('resize', handleResize)
+    } else {
+      return
+    }
+  }, [])
+  return windowSizes
+}
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,10 +1,16 @@
+import { GetWindowSize } from 'hooks/getWindowSize'
+
 type LayoutProps = {
   children?: React.ReactNode
 }
 const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
+  const windowWidth = GetWindowSize().windowWidth
   return (
-    <div className="pointer-events-none min-h-screen flex flex-col font-sen">
-      <main className="mx-auto w-screen overflow-hidden">{children}</main>
+    <div
+      style={{ width: windowWidth }}
+      className="pointer-events-none min-h-full flex flex-col font-sen"
+    >
+      <main className="w-screen overflow-hidden">{children}</main>
     </div>
   )
 }
