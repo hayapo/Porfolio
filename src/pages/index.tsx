f15422c25cf3311cc23da78e7613f301162516bc
hayapo <keep.forward1001@gmail.com>
2022-10-01 19:58:19 +0900

    :art: Topページの要素を全部fixedにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import SideBarContent from 'components/sidebarcontent'
 
 const index: React.FC = () => {
   return (
-    <div className="bg-top-img min-h-screen bg-cover bg-center -z-50 h-[calc(1vh*100)]">
-      <div className="ml-4 text-6xl sm:ml-10 pt-4 sm:pt-6 sm:text-8xl">
+    <div className="bg-top-img min-h-screen bg-cover bg-center -z-50 h-[calc(1vh*100)] fixed w-screen">
+      <div className="fixed ml-4 text-6xl sm:ml-10 pt-4 sm:pt-6 sm:text-8xl">
         Hayapo Portfolio
       </div>
       <SideBarContent />
